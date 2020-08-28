cf5d7f531b7f2b971e3b29b02b8f56a1f287bd7b
nappe404-web <62485371+nappe404-web@users.noreply.github.com>
2020-08-27 22:32:29 -0300

    Landing Page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,56 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import img from "../public/logo.svg";
+import imgGooglePlay from "../public/google-play-badge.png";
+import imgHand from "../public/presentation_image.png";
+import {
+  Container,
+  Header,
+  Ul,
+  Landing,
+  Texts,
+  ImageContainer,
+} from "./styles";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>DevSpace</title>
+        <link rel="icon" type="image/png" href={img} />
       </Head>
+      <main>
+        <Container>
+          <Header>
+            <img src={img} />
+            <Ul>
+              <li>
+                <a href="#">Entrar</a>
+              </li>
+            </Ul>
+          </Header>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <Landing>
+            <Texts>
+              <h1>Uma nova experiência ao buscar novas oportunidades...</h1>
+              <p>
+                Profissionais de <span>Front-End</span>, <span>Back-End</span>,{" "}
+                <span>Mobile</span> e demais áreas, com o <span>DevSpace</span>{" "}
+                você terá a melhor experiência ao buscar uma nova oportunidade.
+              </p>
+            </Texts>
+            <a href="">
+              <img
+                alt="google play logo"
+                src={imgGooglePlay}
+                className="imgGooglePlay"
+              />
+            </a>
+            <ImageContainer>
+              <img alt="Hand Image" src={imgHand} className="imgHand" />
+            </ImageContainer>
+          </Landing>
+        </Container>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
---
+++ pages/styles.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #121421;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    width: 100%;
+    height: 100vh;
+`
+export const Header = styled.div`
+    display: flex;
+	align-items: center;
+    justify-content: space-between;
+    background-color: rgba(18, 20, 33, 0.2);
+    padding: 30px 70px;
+
+    img{
+        width: 149px;
+    }
+`
+
+export const Ul = styled.div`
+    display: flex;
+	width: 15%;
+    justify-content: space-between;
+    list-style: none;
+    
+    li a{
+        font-weight: 700;
+        text-decoration: none;
+        color: white;
+    }
+`
+
+export const Landing = styled.div`
+    margin-left: 70px;
+    max-height: 90vh;
+    .imgGooglePlay{
+        width: 180px;
+        margin-top: 10px;
+    }
+
+`
+
+export const Texts = styled.div`
+    color: white;
+    max-width: 550px;
+    padding-top: 170px;
+    h1{
+        font-size: 1.8em;
+        margin-bottom: 10px;
+    }
+    h2{
+        font-size: 1.5em;
+        font-weight: 700;
+    }
+`
+
+export const ImageContainer = styled.div`
+    position: absolute;
+    top: 20%;
+    left: 50.5%; 
+    transform: scale(1.2);
+    @media(max-width: 768px){
+        display: none;
+    }
+    
+`
